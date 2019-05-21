6abf06bf378286821c67e789b1f7f85ff0748e4d
Tim <tja511@york.ac.uk>
2019-05-21 12:14:22 +0100

    No more omitting frame pointers thanks

--- program.py
+++ program.py
@@ -118,7 +118,7 @@ LIBDIR=''' + gp2_dir + '''/lib\n
 OBJECTS := *.c
 CC=gcc
 
-CFLAGS = -shared -I$(INCDIR) -L$(LIBDIR) -fomit-frame-pointer -O2 -Wall -Wextra -lgp2 -lm -fPIC -fno-semantic-interposition
+CFLAGS = -shared -I$(INCDIR) -L$(LIBDIR) -O2 -Wall -Wextra -lgp2 -lm -fPIC -fno-semantic-interposition
 
 default:	$(OBJECTS)
 		$(CC) $(OBJECTS) $(CFLAGS) -o lib''' + name + '''.so
